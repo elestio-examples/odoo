9b0b4917b922b4deef42eebddaa254d914d5fdbe
AmsellemJoseph <amsellem.joseph@gmail.com>
2023-05-01 12:27:01 +0300

    fix

--- scripts/postInstall.sh
+++ scripts/postInstall.sh
@@ -6,29 +6,15 @@ echo "Waiting for software to be ready ..."
 sleep 30s;
 
 #register the local server in the web ui
-target=$(docker-compose port mealie 3000)
 
+target=$(docker-compose port odoo 8069)
 
-JWT=$(curl http://$target/api/auth/token \
--H 'accept-language: fr-FR' \
--H 'content-type: multipart/form-data; boundary=----WebKitFormBoundaryb3mcOTjCtjAsVY9m' \
--H 'user-agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36' \
---data-raw $'------WebKitFormBoundaryb3mcOTjCtjAsVY9m\r\nContent-Disposition: form-data;name="username"\r\n\r\n'${ADMIN_EMAIL}$'\r\n------WebKitFormBoundaryb3mcOTjCtjAsVY9m\r\nContent-Disposition: form-data; name="password"\r\n\r\nMyPassword\r\n------WebKitFormBoundaryb3mcOTjCtjAsVY9m\r\nContent-Disposition: form-data; name="remember_me"\r\n\r\nfalse\r\n------WebKitFormBoundaryb3mcOTjCtjAsVY9m--\r\n' \
---compressed)
-  
-echo "JWT " ${JWT}
-
-access_token=$(echo $JWT | jq -r '.access_token' )
-
-echo "access_token1 " ${access_token}
-
-
-curl http://${target}/api/users/password \
-  -X 'PUT' \
-  -H 'accept: application/json, text/plain, */*' \
-  -H 'accept-language: fr-FR' \
-  -H 'authorization: Bearer '${access_token}'' \
-  -H 'content-type: application/json' \
-  -H 'user-agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36' \
-  --data-raw '{"currentPassword":"MyPassword","newPassword":"'${ADMIN_PASSWORD}'"}' \
+curl http://${target}/web/database/create \
+  -H 'accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7' \
+  -H 'accept-language: fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7,he;q=0.6' \
+  -H 'cache-control: max-age=0' \
+  -H 'content-type: application/x-www-form-urlencoded' \
+  -H 'upgrade-insecure-requests: 1' \
+  -H 'user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36' \
+  --data-raw 'master_pwd='${ADMIN_PASSWORD}'&name=Odoo&login='${ADMIN_EMAIL}'&password='${ADMIN_PASSWORD}'&phone=&lang=en_US&country_code=' \
   --compressed
