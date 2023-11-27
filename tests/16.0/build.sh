af7ec9d3aa794119dca7406e13e187d2d5f7dcd6
Prem chavhan <pchavhan@logiquad.com>
2023-11-27 20:34:09 +0530

    fix

--- tests/16.0/build.sh
+++ tests/16.0/build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 cp -r 16.0/* ./
-docker buildx build . --output type=docker,name=elestio4test/odoo:latest | docker load
+docker buildx build . --output type=docker,name=elestio4test/odoo:16.0 | docker load
